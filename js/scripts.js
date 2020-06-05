d73ce4bb90d7f680e5c33c08421f4bd1d1668f5d
Chris <cmbrauch00@gmail.com>
2020-06-05 18:12:15 -0400

    Now I am attempting to locate the source of a scripts.js issue

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     $("form#favoriteSeason").submit(function(event) {
         const season = $("#season").val();
         
-
         if (season === "spring") {
             $("#output").text("It sounds like you enjoy lovely weather! Coding may not be for you.");
         
@@ -15,22 +14,5 @@ $(document).ready(function() {
         } else {
             $("#output").text("Coding is probably right up your alley. I would begin with Python!");
         }
-    });
-});
-    
-$(document).ready(function() {
-    $("form#answerQuestions").submit(function(event) {
-        const genre = $("#genre").val();
-
-            if (genre === "comedy") {
-                $("#output2").text("I think that you would enjoy C# as you would not stop laughing at how difficult it is!");
-            
-            } else if (genre === "suspense") {
-                $("#output2").text("swift would be right up your alley!");
-            
-            } else {
-                $("#output2").text("Actually, based on this, you should consider looking into French");
-            }
-        event.preventDefault();
     });
 });
