65757bd988b1f5a306db09a3ab21bc1e2ecb5bff
Chris <cmbrauch00@gmail.com>
2020-06-05 13:23:44 -0400

    add a Seasons question for the user which calls towards scripts.jsand will return an answer on which programming language recommendation

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $("form#favoriteSeason").submit(function(event) {
+        const season = $("#season").val();
+        
+        if (season === "spring") {
+            $("#output").text("It sounds like you enjoy lovely weather! Coding may not be for you.");
+        } else if (season === "summer") {
+            $("#output").text("It sounds like you enjoy lovely weather! Coding may not be for you.");
+        } else if (season === "fall") {
+            $("#output").text("Fall can be great! But I think that Ruby would be well suited for you!");
+        } else {
+            $("#output").text("Coding is probably right up your alley. I would begin with Python");
+        }
+        event.preventDefault();
+    })
+});
