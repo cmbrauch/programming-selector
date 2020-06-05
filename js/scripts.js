f08c3042e8673a8ea867743237866f6ac01b553f
cmbrauch <65939504+cmbrauch@users.noreply.github.com>
2020-06-05 19:03:31 -0400

    manually upload to troubleshoot

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
         const season = $("#season").val();
 
         if (season === "spring") {
-            $("#output").text("Magic 8 ball says: You would enjoy Go!");
+            $("#output").text("It sounds like you enjoy lovely weather! Coding may not be for you.");
         
         } else if (season === "summer") {
-            $("#output").text("It looks as though studying Rust is in your future!");
+            $("#output").text("It sounds like you enjoy hot weather! Coding may not be for you.");
 
         } else if (season === "fall") {
-            $("#output").text("Apparently based on your choices, you should already be efficient at Ruby!");
+            $("#output").text("Fall can be great! But I think that Ruby would be well suited for you!");
 
         } else {
             $("#output").text("Coding is probably right up your alley. I would begin with Python!");
