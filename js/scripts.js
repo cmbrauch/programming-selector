1c0827b341f059fdc5f19679f6bd7039ab7bba42
Chris <cmbrauch00@gmail.com>
2020-06-05 17:28:24 -0400

    add body to html, as well as expand on pulldown options for the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("form#favoriteSeason").submit(function(event) {
         const season = $("#season").val();
-        const genre = $("#genre").val();
+        
 
         if (season === "spring") {
             $("#output").text("It sounds like you enjoy lovely weather! Coding may not be for you.");
@@ -15,6 +15,22 @@ $(document).ready(function() {
         } else {
             $("#output").text("Coding is probably right up your alley. I would begin with Python!");
         }
+    });
+});
+    
+$(document).ready(function() {
+    $("form#questions").submit(function(event) {
+        const genre = $("#genre").val();
+
+            if (genre === "comedy") {
+                $("#output2").text("I think that you would enjoy C# as you would not stop laughing at how difficult it is!");
+            
+            } else if (genre === "suspense") {
+                $("#output2").text("swift would be right up your alley!");
+            
+            } else {
+                $("#output2").text("Actually, based on this, you should consider looking into French");
+            }
         event.preventDefault();
     });
 });
